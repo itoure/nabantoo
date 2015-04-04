11f092f0231a842c5bbf3060f7a1af216243700a
ibou <ib.toure@gmail.com>
2015-04-04 19:14:05 +0200

    init layout

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,11 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+//Route::get('/', 'WelcomeController@index');
 
-Route::get('home', 'HomeController@index');
+//Route::get('home', 'HomeController@index');
+
+Route::controller('home', 'DashboardController');
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
